cfa2849315387750346beab76577287ec3340ffd
kanye <kanye@ushareit.com>
2021-07-16 16:19:36 +0800

    no message

--- shareit/mongdb.py
+++ shareit/mongdb.py
@@ -1,9 +1,5 @@
 from pymongo import MongoClient
 
-mongo_col = MongoClient('127.0.0.1',27017).short_video_label.label_samples
-
-test_mongo_col = MongoClient('127.0.0.1',27017).short_video_label.label_samples_test
-
 
 label_samples_col = MongoClient('127.0.0.1',27017).short_video_label.label_samples
 

a0471457469e270c05bd7ded9450eb3ba236f8ff
kanye <kanye@ushareit.com>
2021-07-20 11:48:21 +0800

    mongo查询工具增加limit

--- utils/mongo_tool.py
+++ utils/mongo_tool.py
@@ -1,6 +1,11 @@
-import  pymongo
+import pymongo
 from tqdm import tqdm
 
-def mongo_progress_bar_iterator(collection,query,filter = None,desc = None):
-    total = collection.count(query)
-    return tqdm(collection.find(query,filter,batch_size=5),desc =desc,total = total)
+
+def mongo_progress_bar_iterator(collection, query, filter=None, desc=None, limit=None):
+	total = collection.count(query)
+	finder = collection.find(query, filter, batch_size=5)
+	if limit:
+		finder = finder.limit(limit)
+	
+	return tqdm(finder, desc=desc, total=total)

1b750a3d32c8bd0b55c044b845353047680ccd2c
kanye <kanye@ushareit.com>
2021-08-31 15:18:59 +0800

    增加色情识别库地址

--- shareit/mongdb.py
+++ shareit/mongdb.py
@@ -4,3 +4,5 @@ from pymongo import MongoClient
 label_samples_col = MongoClient('127.0.0.1',27017).short_video_label.label_samples
 
 unsupervised_sample_col = MongoClient('127.0.0.1',27017).short_video_label.unsupervised_sample
+
+porn_sample_col = MongoClient('127.0.0.1',27017).short_video_label.porn_sample

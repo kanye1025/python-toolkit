871a58993af55f694adc93e18f426014225c8bbc
kanye <kanye@ushareit.com>
2021-09-26 16:35:48 +0800

    增加直接读取xls中数据进行验证的方法

--- utils/image_tool.py
+++ utils/image_tool.py
@@ -1,5 +1,9 @@
-from ..utils.os_tool import list_dir_extend
+#from ..utils.os_tool import list_dir_extend,list_dir_all_files
 import os
+
+from .os_tool import list_dir_extend,list_dir_all_files
+
+
 from PIL import Image
 
 image_exts = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp']
@@ -16,23 +20,9 @@ def resize(src_image_path, dest_image_path,size):
         
         
         
-def check_and_del_image_dir(image_dir):
-    count = 0
-    for _,file_path in list_dir_all_files(image_dir,matching = image_exts,rate_progress=True):
-        try:
-            im = Image.open(file_path)#.convert('RGB')
-            im.resize((10,10)).convert('RGB')
-            if not im:
-                raise Exception('cannot open image_file')
-        except Exception as e:
-            print(e)
-            print(f'delete image file {file_path}')
-            os.remove(file_path)
-            count+=1
-    print(f'remove {count} images')
 
-            
-if __name__ == '__main__':
+
+
    
     
-    check_and_del_image_dir('/data/porn/')
+    

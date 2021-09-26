b8fd872c3b15d6e09937ce1ebe901c5870dc7705
kanye <kanye@ushareit.com>
2021-09-26 17:29:09 +0800

    增加不缩放crop的transform

---
+++ ml/torch_utils/transforms_ex.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import torch
+import random
+
+
+class FiveCropNoResize(torch.nn.Module):
+	direction = ['top-left', 'top-right', 'bottom-left', 'bottom-right', 'center']
+	
+	def __init__(self, crop_rate_min=0.8, crop_rate_max=0.9, p=0.5):
+		super().__init__()
+		self.crop_rate_min = crop_rate_min
+		self.crop_rate_max = crop_rate_max
+		self.p = p
+	
+	def forward(self, img):
+		if torch.rand(1) > self.p:
+			return img
+		d = self.direction[random.randint(0, 4)]
+		
+		crop_rate = random.uniform(self.crop_rate_min, self.crop_rate_max)
+		
+		if 'center' == d:
+			top = img.size[1] * (0.5 - crop_rate / 2)
+			bottom = img.size[1] * (0.5 + crop_rate / 2)
+			left = img.size[0] * (0.5 - crop_rate / 2)
+			right = img.size[0] * (0.5 + crop_rate / 2)
+		else:
+			if 'top' in d:
+				top = 0
+				bottom = img.size[1] * crop_rate
+			elif 'bottom' in d:
+				top = img.size[1] * (1 - crop_rate)
+				bottom = img.size[1]
+			
+			if 'left' in d:
+				left = 0
+				right = img.size[0] * crop_rate
+			elif 'right' in d:
+				left = img.size[0] * (1 - crop_rate)
+				right = img.size[0]
+		
+		box = (int(left), int(top), int(right), int(bottom))
+		print(d, box)
+		return img.crop(box)

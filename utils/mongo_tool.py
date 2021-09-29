304630d4f82ae4ab0647924d2b65f902c37fda31
kanye <kanye@ushareit.com>
2021-09-29 16:12:30 +0800

    优化mongo查询显示条，当指定limit时，进度条total显示实际数量

--- utils/mongo_tool.py
+++ utils/mongo_tool.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 
 
 def mongo_progress_bar_iterator(collection, query, filter=None, sort = None, limit=None,desc=None):
+	
 	total = collection.count(query)
 	finder = collection.find(query, filter, batch_size=5)
 	if sort:
@@ -11,4 +12,4 @@ def mongo_progress_bar_iterator(collection, query, filter=None, sort = None, lim
 		finder = finder.limit(limit)
 	if not desc:
 		desc = collection.name
-	return tqdm(finder, desc=desc, total=total)
+	return tqdm(finder, desc=desc, total=min(total,limit))

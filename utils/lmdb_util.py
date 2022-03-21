847677514b89b72f1399fb6bca6d1dbb9a779cda
kanye <kanye@ushareit.com>
2022-03-21 15:40:00 +0800

    增加lmdb处理代码库

---
+++ utils/lmdb_util.py
@@ -0,0 +1,22 @@
+from io import BytesIO
+from PIL import Image
+import lmdb
+from toolkit.utils.url_tool import get_data_from_url
+
+def donwload_image_to_lmdb(lmdb_env,url,key,resize = None,recover = False):
+	if not recover:
+		txn = lmdb_env.begin(write=False)
+		buf = txn.get(key.encode(),None)
+		if buf:
+			return
+		
+	buf = get_data_from_url(url)
+	if resize:
+		img = Image.open(BytesIO(buf)).resize(resize, Image.ANTIALIAS)
+		data_byte = BytesIO()
+		img.save(data_byte, format='webp')
+		buf = data_byte.getbuffer()
+	txn = lmdb_env.begin(write=True)
+	txn.put(key.encode(),buf)
+	txn.commit()
+	

2645598b30ad489b7b142e22fe8a711620db2cfc
kanye <kanye@ushareit.com>
2022-03-22 11:14:40 +0800

    增加dir_to_lmdb

--- utils/lmdb_util.py
+++ utils/lmdb_util.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from PIL import Image
 import lmdb
 from toolkit.utils.url_tool import get_data_from_url
-
+from toolkit.utils.os_tool import list_dir_extend
 def donwload_image_to_lmdb(lmdb_env,url,key,resize = None,recover = False):
 	if not recover:
 		txn = lmdb_env.begin(write=False)
@@ -20,3 +20,15 @@ def donwload_image_to_lmdb(lmdb_env,url,key,resize = None,recover = False):
 	txn.put(key.encode(),buf)
 	txn.commit()
 	
+	
+def dir_to_lmdb(lmdb_env,file_dir):
+	
+	for file_name,file_path in list_dir_extend(file_dir):
+		with open(file_path, 'br') as f :
+			buf = f.read()
+			if buf:
+				txn = lmdb_env.begin(write=True)
+				txn.put(file_name.encode(), buf)
+				txn.commit()
+			
+		

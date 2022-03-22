f58be6fc2f1c957d9c840d9ddb158eceaddfe43f
kanye <kanye@ushareit.com>
2022-03-22 11:13:11 +0800

    增加logo_bg_lmdb

--- shareit/lmdbs.py
+++ shareit/lmdbs.py
@@ -16,3 +16,12 @@ def get_short_frames_env_lmdb():
 	if not _short_frames_env:
 		_short_frames_env  = lmdb.open("/data/short_frames.lmdb",map_size=3*1024*1024*1024*1024)
 	return _short_frames_env
+
+
+
+_logo_bg_env = None
+def get_logo_bg_env_lmdb():
+	global _logo_bg_env
+	if not _logo_bg_env:
+		_logo_bg_env  = lmdb.open("/data/logo_bg.lmdb",map_size=3*1024*1024*1024*1024)
+	return _logo_bg_env
